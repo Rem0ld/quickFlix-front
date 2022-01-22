1160838ce30f6f7500660180c37eb122e8351f8d
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2022-01-22 11:59:41 +0100

    exporting types

---
+++ src/types.d.ts
@@ -0,0 +1,39 @@
+export type Pagination<T> = {
+  total: number;
+  skip: number;
+  limit: number;
+  data: T[];
+};
+
+export type Video = {
+  _id: string;
+  name: string;
+  location: string;
+  ext: string;
+  subtitles: string[];
+};
+
+export type Episode = { number: string, ref: Video | string }
+export type Season = {
+  number: string,
+  episodes: Episode[],
+}
+export type TvShow = {
+  idMovieDb?: string;
+  name: string;
+  numberSeason?: number;
+  numberEpisode?: number;
+  seasons: Season[],
+  ongoing?: boolean;
+  originCountry?: string;
+  posterPath?: string;
+  resume?: string;
+  score?: number;
+  date?: Date;
+  trailerYtCode?: string;
+};
+
+export type ActionReducer = {
+  type: string;
+  value?: any;
+};
