cef3798148eb11e96129333c9231d097886f38f6
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2022-01-24 22:52:39 +0100

    WIP player

--- src/routes/Player/Player.tsx
+++ src/routes/Player/Player.tsx
@@ -11,7 +11,7 @@ import { Popover, Slider } from "antd";
 import "antd/dist/antd.css";
 
 const baseUrl = `http://${import.meta.env.DEV ? "localhost" : "192.168.0.11"}:3050/api/`;
-const size = 44;
+const size = 35;
 
 const Player = () => {
   const { id } = useParams();
@@ -21,6 +21,11 @@ const Player = () => {
   const progressBarRef = useRef<any>(null);
 
   const [isPlaying, setIsPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [leftTime, setLeftTime] = useState(0);
+  const [hours, setHours] = useState("");
+  const [mins, setMins] = useState("");
+  const [secs, setSecs] = useState("");
 
   const play = () => {
     videoRef.current.play();
@@ -40,9 +45,40 @@ const Player = () => {
     videoRef.current.volume = value / 100;
   };
 
+  const isFullScreen = function () {
+    return !!(
+      document.fullscreen ||
+      document.webkitIsFullScreen ||
+      document.mozFullScreen ||
+      document.msFullscreenElement ||
+      document.fullscreenElement
+    );
+  };
+
+  const setFullscreenData = function (state: any) {
+    videoRef.current.setAttribute("data-fullscreen", !!state);
+  };
+
+  const handleFullScreen = () => {
+    if (isFullScreen()) {
+      if (document.exitFullscreen) document.exitFullscreen();
+      else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+      else if (document.webkitCancelFullScreen) document.webkitCancelFullScreen();
+      else if (document.msExitFullscreen) document.msExitFullscreen();
+      setFullscreenData(false);
+    } else {
+      if (videoRef.current.requestFullscreen) videoRef.current.requestFullscreen();
+      else if (videoRef.current.mozRequestFullScreen) videoRef.current.mozRequestFullScreen();
+      else if (videoRef.current.webkitRequestFullScreen) videoRef.current.webkitRequestFullScreen();
+      else if (videoRef.current.msRequestFullscreen) videoRef.current.msRequestFullscreen();
+      setFullscreenData(true);
+    }
+  };
+
   useEffect(() => {
     const loadedMetadata = videoRef.current.addEventListener("loadedmetadata", () => {
       console.log(videoRef.current.duration);
+      setDuration(videoRef.current.duration);
       progressRef.current.setAttribute("max", videoRef.current.duration);
     });
 
@@ -53,6 +89,8 @@ const Player = () => {
       progressRef.current.value = videoRef.current.currentTime;
       progressBarRef.current.style.with =
         Math.floor((videoRef.current.currentTime / videoRef.current.duration) * 100) + "%";
+
+      setLeftTime(+duration - videoRef.current.currentTime);
     });
 
     // @ts-ignore
@@ -62,6 +100,19 @@ const Player = () => {
       videoRef.current.currentTime = pos * videoRef.current.duration;
     });
 
+    // document.addEventListener("fullscreenchange", function () {
+    //   setFullscreenData(!!(document.fullscreen || document.fullscreenElement));
+    // });
+    // document.addEventListener("webkitfullscreenchange", function () {
+    //   setFullscreenData(!!document.webkitIsFullScreen);
+    // });
+    // document.addEventListener("mozfullscreenchange", function () {
+    //   setFullscreenData(!!document.mozFullScreen);
+    // });
+    // document.addEventListener("msfullscreenchange", function () {
+    //   setFullscreenData(!!document.msFullscreenElement);
+    // });
+
     () => {
       videoRef.current.removeEventListener("loadedmetadata", loadedMetadata);
       videoRef.current.removeEventListener("timeupdate", timeUpdate);
@@ -70,7 +121,7 @@ const Player = () => {
   });
 
   return (
-    <div>
+    <div className="h-screen w-screen relative bg-black">
       <MdKeyboardBackspace
         onClick={() => {
           navigate(-1);
@@ -79,20 +130,26 @@ const Player = () => {
         size={size}
         className="absolute z-50 left-6 top-4 cursor-pointer"
       />
-      <video ref={videoRef} width="100%" height="100%" preload="metadata">
-        <source src={`${baseUrl}${id}`} type="video/webm" />
-        <source src={`${baseUrl}${id}`} type="video/mp4" />
-        <source src={`${baseUrl}${id}`} type='video/mp4; codecs="avc1"' />
-      </video>
-      <div id="controls" className="h-20 flex flex-col justify-center px-5 bg-black">
-        <progress
-          ref={progressRef}
-          id="progress"
-          value="0"
-          className="w-full h-1 rounded-full mb-1 text-red-500 cursor-pointer"
-        >
-          <span id="progressBar" ref={progressBarRef} className="progressBar bg-red-500" />
-        </progress>
+      <div className="relative">
+        <video ref={videoRef} className="h-screen absolute" preload="metadata">
+          <source src={`${baseUrl}${id}`} type="video/webm" />
+          <source src={`${baseUrl}${id}`} type="video/mp4" />
+          <source src={`${baseUrl}${id}`} type='video/mp4; codecs="avc1"' />
+        </video>
+      </div>
+      <div
+        id="controls"
+        className="h-20 w-full flex flex-col justify-center absolute bottom-0 px-5 bg-black"
+      >
+        <div className="flex gap-x-2 items-center">
+          <progress
+            ref={progressRef}
+            value="0"
+            className="w-full h-1 rounded-full mb-1 text-red-500 cursor-pointer"
+          >
+            <span id="progressBar" ref={progressBarRef} className="progressBar bg-red-500" />
+          </progress>
+        </div>
         <div className="flex items-center justify-between">
           <div className="flex gap-x-4">
             {isPlaying ? (
@@ -153,7 +210,7 @@ const Player = () => {
             <button id="speed">
               <SiSpeedtest color="white" size={size} />
             </button>
-            <button id="fullscreen">
+            <button id="fullscreen" onClick={handleFullScreen}>
               <RiFullscreenLine color="white" size={size} />
             </button>
           </div>
