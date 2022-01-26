9501348eec33e336c20f6e1c7ccfa1cdad66dfd3
Rem0ld <60188728+Rem0ld@users.noreply.github.com>
2022-01-26 18:59:08 +0100

    WIP player

---
+++ src/hooks/UseControlPlayer.ts
@@ -0,0 +1,124 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import React, { useEffect, useState } from "react";
+
+
+export default function UseControlPlayer(videoRef: any, videoContainer: any, progressRef: any, progressBarRef: any) {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isFullScreen, setIsFullScreen] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [leftTime, setLeftTime] = useState(0);
+
+  const play = () => {
+    videoRef.current.play();
+    setIsPlaying(true);
+  };
+
+  const pause = () => {
+    videoRef.current.pause();
+    setIsPlaying(false);
+  };
+
+  const volumeOff = () => {
+    videoRef.current.muted = !videoRef.current.muted;
+  };
+
+  const changeVolume = (value: number) => {
+    videoRef.current.volume = value / 100;
+  };
+
+  const setFullscreenData = function (state: any) {
+    videoContainer.current.setAttribute("data-fullscreen", !!state);
+  };
+
+  const handleFullScreen = () => {
+    if (isFullScreen) {
+      if (document.exitFullscreen) document.exitFullscreen();
+      else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+      else if (document.webkitCancelFullScreen) document.webkitCancelFullScreen();
+      else if (document.msExitFullscreen) document.msExitFullscreen();
+      setFullscreenData(false);
+      setIsFullScreen(false);
+    } else {
+      if (videoContainer.current.requestFullscreen) videoContainer.current.requestFullscreen();
+      else if (videoContainer.current.mozRequestFullScreen)
+        videoContainer.current.mozRequestFullScreen();
+      else if (videoContainer.current.webkitRequestFullScreen)
+        videoContainer.current.webkitRequestFullScreen();
+      else if (videoContainer.current.msRequestFullscreen)
+        videoContainer.current.msRequestFullscreen();
+      setFullscreenData(true);
+      setIsFullScreen(true);
+    }
+  };
+
+  /**
+  * Setting up all events to take care of the video
+  */
+  useEffect(() => {
+    const loadedMetadata = videoRef.current.addEventListener("loadedmetadata", () => {
+      console.log(videoRef.current.duration);
+      setDuration(videoRef.current.duration);
+      progressRef.current.setAttribute("max", videoRef.current.duration);
+    });
+
+    const timeUpdate = videoRef.current.addEventListener("timeupdate", () => {
+      // if (!progressRef.current.getAttribute("max"))
+      //   progressRef.current.setAttribute("max", videoRef.current.duration);
+
+      // progressRef.current.value = videoRef.current.currentTime;
+      // TODO: onclick UP we fire the event, when we look for another frame in the progress bar we update the UI to have this feeling of going smoothly
+      // but we only fire event on mouse up
+      progressBarRef.current.style.width =
+        Math.floor((videoRef.current.currentTime / videoRef.current.duration) * 100) + "%";
+
+      setLeftTime(+duration - videoRef.current.currentTime);
+    });
+
+    // @ts-ignore
+    const progress = progressRef.current.addEventListener("click", (e) => {
+      const rect = progressRef.current.getBoundingClientRect();
+      const pos = (e.pageX - rect.left) / progressRef.current.offsetWidth;
+      console.log(pos);
+      videoRef.current.currentTime = pos * videoRef.current.duration;
+    });
+
+    const fsChange = document.addEventListener("fullscreenchange", function () {
+      setFullscreenData(!!(document.fullscreen || document.fullscreenElement));
+    });
+    const webkitFsChange = document.addEventListener("webkitfullscreenchange", function () {
+      setFullscreenData(!!document.webkitIsFullScreen);
+    });
+    const mozFsChange = document.addEventListener("mozfullscreenchange", function () {
+      setFullscreenData(!!document.mozFullScreen);
+    });
+    const msFsChange = document.addEventListener("msfullscreenchange", function () {
+      setFullscreenData(!!document.msFullscreenElement);
+    });
+
+    () => {
+      videoRef.current.removeEventListener("loadedmetadata", loadedMetadata);
+      videoRef.current.removeEventListener("timeupdate", timeUpdate);
+      progressRef.current.removeEventListener("click", progress);
+      // @ts-expect-error
+      document.removeEventListener("fullscreenchange", fsChange);
+      // @ts-expect-error
+      document.removeEventListener("webkitfullscreenchange", webkitFsChange);
+      // @ts-expect-error
+      document.removeEventListener("mozfullscreenchange", mozFsChange);
+      // @ts-expect-error
+      document.removeEventListener("msfullscreenchange", msFsChange);
+    };
+  }, []);
+
+  return {
+    play,
+    isPlaying,
+    pause,
+    volumeOff,
+    changeVolume,
+    duration,
+    leftTime,
+    handleFullScreen,
+    isFullScreen
+  }
+}
--- src/routes/Player/Player.tsx
+++ src/routes/Player/Player.tsx
@@ -9,7 +9,8 @@ import { RiFullscreenLine, RiFullscreenExitLine } from "react-icons/ri";
 import { MdKeyboardBackspace } from "react-icons/md";
 import { Popover, Slider } from "antd";
 import "antd/dist/antd.css";
-import { setInterval } from "timers/promises";
+import gsap from "gsap";
+import UseControlPlayer from "../../hooks/UseControlPlayer";
 
 const baseUrl = `http://${import.meta.env.DEV ? "localhost" : "192.168.0.11"}:3050/api/`;
 const size = 30;
@@ -19,140 +20,114 @@ const Player = () => {
   const navigate = useNavigate();
   const videoRef = useRef<any>(null);
   const videoContainer = useRef<any>(null);
+  const controlRef = useRef<any>(null);
+  const goBackRef = useRef<any>(null);
   const progressRef = useRef<any>();
   const progressBarRef = useRef<any>(null);
 
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [isFullScreen, setIsFullScreen] = useState(false);
-  const [duration, setDuration] = useState(0);
-  const [leftTime, setLeftTime] = useState(0);
+  const {
+    play,
+    isPlaying,
+    pause,
+    volumeOff,
+    changeVolume,
+    duration,
+    leftTime,
+    handleFullScreen,
+    isFullScreen,
+  } = UseControlPlayer(videoRef, videoContainer, progressRef, progressBarRef);
+
   const [hours, setHours] = useState("");
   const [mins, setMins] = useState("");
   const [secs, setSecs] = useState<string | number>("");
   const [controlsAreVisible, setControlsAreVisible] = useState(false);
+  const [timerControlId, setTimerControlId] = useState<NodeJS.Timeout>();
+  const [mouseStopMoving, setMouseStopMoving] = useState<boolean>();
 
   // TODO: make api call to get info on video, if movie we hide the next btn
 
-  const play = () => {
-    videoRef.current.play();
-    setIsPlaying(true);
+  const setTimeOut = () => {
+    const id = setTimeout(() => {
+      setControlsAreVisible(false);
+    }, 2000);
+
+    setTimerControlId(id);
   };
 
-  const pause = () => {
-    videoRef.current.pause();
-    setIsPlaying(false);
-  };
+  const handleHoverMediaPlayer = () => {
+    if (timerControlId) clearTimeout(timerControlId);
+    if (mouseStopMoving) setMouseStopMoving(false);
+    if (!controlsAreVisible) setControlsAreVisible(true);
 
-  const volumeOff = () => {
-    videoRef.current.muted = !videoRef.current.muted;
-  };
-
-  const changeVolume = (value: number) => {
-    videoRef.current.volume = value / 100;
-  };
-
-  const setFullscreenData = function (state: any) {
-    videoContainer.current.setAttribute("data-fullscreen", !!state);
-  };
-
-  const handleFullScreen = () => {
-    if (isFullScreen) {
-      if (document.exitFullscreen) document.exitFullscreen();
-      else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
-      else if (document.webkitCancelFullScreen) document.webkitCancelFullScreen();
-      else if (document.msExitFullscreen) document.msExitFullscreen();
-      setFullscreenData(false);
-      setIsFullScreen(false);
-    } else {
-      if (videoContainer.current.requestFullscreen) videoContainer.current.requestFullscreen();
-      else if (videoContainer.current.mozRequestFullScreen)
-        videoContainer.current.mozRequestFullScreen();
-      else if (videoContainer.current.webkitRequestFullScreen)
-        videoContainer.current.webkitRequestFullScreen();
-      else if (videoContainer.current.msRequestFullscreen)
-        videoContainer.current.msRequestFullscreen();
-      setFullscreenData(true);
-      setIsFullScreen(true);
-    }
+    setTimeout(() => {
+      setMouseStopMoving(true);
+    }, 500);
   };
 
   useEffect(() => {
-    const loadedMetadata = videoRef.current.addEventListener("loadedmetadata", () => {
-      console.log(videoRef.current.duration);
-      setDuration(videoRef.current.duration);
-      progressRef.current.setAttribute("max", videoRef.current.duration);
-    });
+    if (mouseStopMoving) {
+      // TODO: get boundingrectclient of controls and go back button + get mouse position if not on these ones, we can set timeout to disappear
+    }
+  }, [mouseStopMoving]);
 
-    const timeUpdate = videoRef.current.addEventListener("timeupdate", () => {
-      // if (!progressRef.current.getAttribute("max"))
-      //   progressRef.current.setAttribute("max", videoRef.current.duration);
-
-      // progressRef.current.value = videoRef.current.currentTime;
-      // TODO: onclick UP we fire the event, when we look for another frame in the progress bar we update the UI to have this feeling of going smoothly
-      // but we only fire event on mouse up
-      progressBarRef.current.style.width =
-        Math.floor((videoRef.current.currentTime / videoRef.current.duration) * 100) + "%";
-
-      setLeftTime(+duration - videoRef.current.currentTime);
-    });
-
-    // @ts-ignore
-    const progress = progressRef.current.addEventListener("click", (e) => {
-      const rect = progressRef.current.getBoundingClientRect();
-      const pos = (e.pageX - rect.left) / progressRef.current.offsetWidth;
-      console.log(pos);
-      videoRef.current.currentTime = pos * videoRef.current.duration;
-    });
-
-    const fsChange = document.addEventListener("fullscreenchange", function () {
-      setFullscreenData(!!(document.fullscreen || document.fullscreenElement));
-    });
-    const webkitFsChange = document.addEventListener("webkitfullscreenchange", function () {
-      setFullscreenData(!!document.webkitIsFullScreen);
-    });
-    const mozFsChange = document.addEventListener("mozfullscreenchange", function () {
-      setFullscreenData(!!document.mozFullScreen);
-    });
-    const msFsChange = document.addEventListener("msfullscreenchange", function () {
-      setFullscreenData(!!document.msFullscreenElement);
-    });
-
-    () => {
-      videoRef.current.removeEventListener("loadedmetadata", loadedMetadata);
-      videoRef.current.removeEventListener("timeupdate", timeUpdate);
-      progressRef.current.removeEventListener("click", progress);
-      // @ts-expect-error
-      document.removeEventListener("fullscreenchange", fsChange);
-      // @ts-expect-error
-      document.removeEventListener("webkitfullscreenchange", webkitFsChange);
-      // @ts-expect-error
-      document.removeEventListener("mozfullscreenchange", mozFsChange);
-      // @ts-expect-error
-      document.removeEventListener("msfullscreenchange", msFsChange);
-    };
-  });
+  useEffect(() => {
+    if (!controlsAreVisible) {
+      gsap.to(goBackRef.current, {
+        opacity: 0,
+      });
+      gsap.to(controlRef.current, {
+        bottom: "-80px",
+      });
+    } else {
+      gsap.to(goBackRef.current, {
+        opacity: 1,
+      });
+      gsap.to(controlRef.current, {
+        bottom: "0px",
+      });
+    }
+  }, [controlsAreVisible]);
 
   // TODO: make a useEffect with timeInterval of 1sec for updating time
 
   return (
-    <div ref={videoContainer} className="h-screen w-screen relative bg-black">
-      <MdKeyboardBackspace
-        onClick={() => {
-          navigate(-1);
+    <div
+      onMouseMove={handleHoverMediaPlayer}
+      ref={videoContainer}
+      className="h-screen w-screen relative bg-black"
+    >
+      <span
+        ref={goBackRef}
+        onMouseOver={() => {
+          if (timerControlId) clearTimeout(timerControlId);
         }}
-        color="white"
-        size={size}
-        className="absolute z-50 left-6 top-4 cursor-pointer"
-      />
+      >
+        <MdKeyboardBackspace
+          onClick={() => {
+            navigate(-1);
+          }}
+          color="white"
+          size={size + 5}
+          className="absolute z-50 left-6 top-4 cursor-pointer"
+        />
+      </span>
       <video ref={videoRef} className="h-screen absolute" preload="metadata">
         <source src={`${baseUrl}${id}`} type="video/webm" />
         <source src={`${baseUrl}${id}`} type="video/mp4" />
         <source src={`${baseUrl}${id}`} type='video/mp4; codecs="avc1"' />
       </video>
       <div
-        id="controls"
+        ref={controlRef}
+        onMouseEnter={() => {
+          console.log(timerControlId);
+          if (timerControlId) clearTimeout(timerControlId);
+        }}
+        onMouseOver={() => {
+          if (timerControlId) clearTimeout(timerControlId);
+        }}
         className="h-20 w-full flex flex-col gap-y-2 justify-center absolute bottom-0 px-5 bg-black"
       >
+        {/* === PROGRESS BAR === */}
         <div className="flex items-center">
           <div
             ref={progressRef}
@@ -166,6 +141,7 @@ const Player = () => {
             </div>
           </div>
         </div>
+        {/* === MAIN CONTROLS === */}
         <div className="flex items-center justify-between">
           <div className="flex gap-x-4">
             {isPlaying ? (
